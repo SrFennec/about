df9666d4971fe95ff724cf90fc886963a2495a39
SrFennec <105950466+SrFennec@users.noreply.github.com>
2023-08-26 03:59:54 -0300

    Add files via upload

--- main_script.js
+++ main_script.js
@@ -1,4 +1,8 @@
 
+
+const image_array = [[0]]
+
+
 window.onload = function(){
      /*
      if(localStorage.getItem("mode") != "dark-mode"){
@@ -50,16 +54,74 @@ function mode(){
     }
 
 }
-function random_splash(){
-     var i = Math.round(randomNumber(0,2));
-     do{
-          i = Math.round(randomNumber(0,2)); 
-     }while(document.getElementById("img"+i))
+
+function setup_splash(str, amount, id){
+     var arr = image_array[id];
+     arr[0] = 0;
+     arr[1] = str;
+     arr[2] = true;
+     var image_order = [];
+     for(i = 0; i < amount; i++){
+          image_order[i] = i;
+     }
      
-     document.getElementById("imgslider").innerHTML = '<center><img id="img'+i+'" style="width: 640px; height: 360px;" src="images/splash_'+i+'.png"></center>';
+     image_order = shuffle_array(image_order);
+     
+     for(i = 0; i < amount; i++){
+          arr[i+3] = image_order[i];
+     }
+     
+     image_array[id] = arr;
+
+
 }
+function get_splash(str, amount,id,rearrange){
+
+     var arr = image_array[id]
+     var current = arr[0]+1;
+     if(arr[2] == undefined){
+          setup_splash(str, amount, id)
+     }
+     if(current >= arr.length-3){
+          if(rearrange){
+               setup_splash(str, amount, id)
+          }
+
+          current = 0;
+     }
+     
+     
+     var str = arr[1];
+     
+
+     arr[0] = current;
+     var img = arr[current+3]
+     
+     document.getElementById("imgslider").innerHTML = '<img data-aos="flip-up" id="img.'+id+"."+current+'" src="images/'+str+img+'.png">';
+
+     image_array[id] = arr;
+}
+
+function shuffle_array(array) {
+     for (let i = array.length - 1; i > 0; i--) {
+         const j = Math.floor(Math.random() * (i + 1));
+         [array[i], array[j]] = [array[j], array[i]];
+     }
+
+     return array;
+ }
+
+
 
 
 function randomNumber(min, max) {
      return Math.random() * (max - min) + min;
  }
+
+
+
+
+
+
+
+ 
